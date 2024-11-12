a70674e01dbe6dcbabe478222d64a47d83ac97ef
DanielConduri <jonathand.tene@espoch.edu.ec>
2024-11-12 09:26:13 -0500

    update yml file 4

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ async function main(){
     try {
         await sequelize.sync( { force: false }); //No recrea las tablas
         console.log('Connection has been established successfully.');
-        app.listen(3001)
-        console.log('Server is  listening on port', 3001)
+        app.listen(8000)
+        console.log('Server is  listening on port', 8000)
     } catch (error) {
         console.error('Unable to connect to the database:', error);
     }
