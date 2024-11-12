378452f2d4b3609bffc2d7775d98bebe2ebfb157
DanielConduri <jonathand.tene@espoch.edu.ec>
2024-11-11 21:29:17 -0500

    configuration

--- src/database/database.js
+++ src/database/database.js
@@ -1,13 +1,16 @@
 import { Sequelize } from 'sequelize';
+import { configVariables } from '../config/variables.config.js';
 
 //Servidor PostgreSQL 15
 export const sequelize = new Sequelize(
-    'db_ventas',
-    'postgres',
-    'backend',
+    configVariables.dbName,
+    configVariables.dbUser,
+    configVariables.dbPassword,
     {
-        host: 'localhost',
-        dialect: 'postgres'
+        host: configVariables.dbServer,
+        dialect: configVariables.dbDialect,
+        logging: false,
+        port: configVariables.dbPort,
     }
-);
+  );
 
